1218f27ebba908da79e03b9a072c725a66516ebd
Nargiza Satybaldieva <semeteeva.n@gmail.com>
2025-02-13 23:47:58 +0600

    added heroes

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,12 +1,12 @@
 import {Heading}  from "../(marketing)/_components/heading";
-
+import { Heroes } from "./_components/heroes";
 const MarketingPage = () => {
   return (
     <div className="min-h-full flex flex-col">
       <div className="flex flex-col items-center justify-center
       md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
         <Heading/>
-        {/* <Heroes /> */}
+        <Heroes />
 
       </div>
     </div>  
