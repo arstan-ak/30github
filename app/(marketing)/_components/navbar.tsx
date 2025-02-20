58c71a6a3e72b9f760785ce9039cc1f8b069972d
Nargiza Satybaldieva <semeteeva.n@gmail.com>
2025-02-20 11:13:19 +0600

    use spinner component

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -7,10 +7,11 @@ import { Logo } from "./logo";
 import { useConvexAuth } from "convex/react";
 import { SignInButton } from "@clerk/clerk-react";
 import { Button } from "@/components/ui/button";
-import { Ghost } from "lucide-react";
+import { Spinner } from "@/components/spinner";
+
 
 export const Navbar = () => {
-    const {isAuthenticated, isLoading} = useConvexAuth()
+    const {isAuthenticated, isLoading} = useConvexAuth();
     const scrolled = useScrollTop();
     return(
         <div className={cn("z-50 bg-background fixed top-0 flex items-center w-full p-6", 
@@ -19,7 +20,7 @@ export const Navbar = () => {
             <Logo/>
             <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
                 {isLoading &&(
-                    <p>Loading...</p>
+                    <Spinner/>
                 )}
                 {!isAuthenticated && !isLoading &&(
                     <>
