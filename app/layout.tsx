c9ebd9551c885d3d690f7b9f6de4568e8b6ea6be
Nargiza Satybaldieva <semeteeva.n@gmail.com>
2025-02-20 11:08:18 +0600

    wrapped with component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { url } from "inspector";
+import { ConvexProviderWithAuth } from "convex/react";
+import { ConvexClientProvider } from "@/components/providers/convex-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +33,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ConvexClientProvider>
+
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
@@ -38,5 +42,6 @@ export default function RootLayout({
         {children}
       </body>
     </html>
+    </ConvexClientProvider>
   );
 }
