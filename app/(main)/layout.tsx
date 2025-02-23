7ed447734a33723547fe09cd51a4094c5a282580
Nargiza Satybaldieva <semeteeva.n@gmail.com>
2025-02-23 21:21:52 +0600

    corrected and added redirect and navigation

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,11 +1,32 @@
 "use client";
 import {ReactNode} from 'react'
 import { useConvexAuth } from 'convex/react';
+import { Spinner } from '@/components/spinner';
+import { redirect } from 'next/navigation';
+import { Navigation } from '../(main)/_components/navigation';
 
 const MainLayout = (children:ReactNode) => {
 
 const {isAuthenticated, isLoading} = useConvexAuth();
-    return 
+if(isLoading){
+    return (
+        <div className='h-full flex items-center justify-center'>
+        <Spinner size="lg"/>
+        </div>
+    )
+}
+if(!isAuthenticated){
+    return redirect("/")
+}
+return(
+    <div className='h-full flex'>
+        <Navigation/>
+        <main className='flex-1 h-full overflow-y-auto'>
+
+        {children}
+        </main>
+        </div>
+)
 }
 
 export default MainLayout
