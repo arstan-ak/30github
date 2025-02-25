c00fb637e757fbdd90455752c56e8d88a5e7db38
Nargiza Satybaldieva <semeteeva.n@gmail.com>
2025-02-25 22:19:39 +0600

    added MenuIcon

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ChevronLeft } from "lucide-react";
+import { ChevronLeft, MenuIcon } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
@@ -27,7 +27,10 @@ export const Navigation =() =>{
         >
             <div 
             role="button"
-            className="h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition">
+            className={cn("h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition",
+                isMobile && "opacity-100"
+            )}
+            >
                 <ChevronLeft className="h-6 w-6"/>
 
             </div>
@@ -42,6 +45,16 @@ export const Navigation =() =>{
             transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0"
             />
         </aside>
+        <div
+        ref={navbarRef}
+        className={cn("absolute top-0 z-[99999] left-60 w-[calc(100%-240px)]",
+            isResetting && "transition-all ease-in-out duration-300",
+            isMobile && "left-0 w-full"
+        )}>
+            <nav>
+                {isCollapsed && <MenuIcon role="button" className="h-6 w-6 text-muted-foreground"/>}
+            </nav>
+        </div>
         </>
     )
 }
