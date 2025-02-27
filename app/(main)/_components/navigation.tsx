30afc61008b5dbb866bc4153197b18c240e045ff
Nargiza Satybaldieva <semeteeva.n@gmail.com>
2025-02-27 23:27:03 +0600

    added collapse function

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -68,6 +68,26 @@ export const Navigation =() =>{
             
         }
     }
+
+    const collapse = () =>{
+    if(sidebarRef.current && navbarRef.current){
+        setIsCollapsed(true)
+        setIsResetting(true)
+
+        sidebarRef.current.style.width = "0";
+        navbarRef.current.style.setProperty(
+            "width",
+            "100%"
+        );
+
+        navbarRef.current.style.setProperty(
+            "left",
+            "0"
+        );
+        setTimeout(()  => setIsResetting(false), 300);
+        
+    }
+}
     return(
         <>
         <aside 
@@ -79,6 +99,7 @@ export const Navigation =() =>{
         )}
         >
             <div 
+            onClick={collapse}
             role="button"
             className={cn("h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition",
                 isMobile && "opacity-100"
@@ -107,7 +128,7 @@ export const Navigation =() =>{
             isMobile && "left-0 w-full"
         )}>
             <nav className="bg-transparent px-3 py-2 w-full">
-                {isCollapsed && <MenuIcon role="button" className="h-6 w-6 text-muted-foreground"/>}
+                {isCollapsed && <MenuIcon onClick={resetWidth} role="button" className="h-6 w-6 text-muted-foreground"/>}
             </nav>
         </div>
         </>
