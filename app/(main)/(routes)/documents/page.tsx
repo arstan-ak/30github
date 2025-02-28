6507c06f679737bfaf9a66aa87562156c3c92ff8
Nargiza Satybaldieva <semeteeva.n@gmail.com>
2025-02-28 22:34:04 +0600

    added empty and add button

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,9 +1,22 @@
+"use client";
+import Image from "next/image";
+import { useUser } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
 const DocumentsPage = () => {
-    return(
-        <div>
-        Document Page
-        </div>
-    )
-}
+  const { user } = useUser();
+  return (
+    <div className="h-full flex flex-col items-center justify-center">
+      <Image src="/empty-box.jpg" height="300" width="300" alt="empty" />
+      <h2 className="font-lg font-medium">
+        Wellcome to {user?.firstName}&apos;s Notion
+      </h2>
+      <button>
+        <PlusCircle className="h-4 w-4 mr-2" />
+        Create a note
+      </button>
+    </div>
+  );
+};
 
-export default DocumentsPage
+export default DocumentsPage;
