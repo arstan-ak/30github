3006c812c17be8b62bfcaa0460ba60c37839f300
Arstan Akatov <akatovx@gmail.com>
2025-03-01 23:41:56 +0600

    added new feature

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const geistSans = Geist({
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
+const geistMono =  Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
